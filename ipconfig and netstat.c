0877af02db98a1c5ebea56a29f1e2e1878f0e570
Epicz0n <138006572+Epicz0n@users.noreply.github.com>
2025-03-24 03:33:24 +0400

    Update ipconfig and netstat.c

--- ipconfig and netstat.c
+++ ipconfig and netstat.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 int main() {
-    // Use the "start" command to open a new Command Prompt window and run ipconfig
+    
     system("start cmd /k ipconfig");
 
-    // Use the "start" command to open a new Command Prompt window and run netstat -na
+    
     system("start cmd /k netstat -na");
     
     system("start cmd /k dir");
